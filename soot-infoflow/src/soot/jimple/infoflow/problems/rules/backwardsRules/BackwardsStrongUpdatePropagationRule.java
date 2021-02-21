eda94abda1c8f5ea6e2a8df4cc3b56fa9838b6bd
Tim Lange <mail@tim-lange.me>
2021-02-21 18:57:44 +0100

    better strong update

--- soot-infoflow/src/soot/jimple/infoflow/problems/rules/backwardsRules/BackwardsStrongUpdatePropagationRule.java
+++ soot-infoflow/src/soot/jimple/infoflow/problems/rules/backwardsRules/BackwardsStrongUpdatePropagationRule.java
@@ -1,16 +1,20 @@
 package soot.jimple.infoflow.problems.rules.backwardsRules;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
 
 import soot.*;
 import soot.jimple.*;
 import soot.jimple.infoflow.InfoflowManager;
 import soot.jimple.infoflow.aliasing.Aliasing;
 import soot.jimple.infoflow.data.Abstraction;
+import soot.jimple.infoflow.data.AccessPath;
 import soot.jimple.infoflow.problems.TaintPropagationResults;
 import soot.jimple.infoflow.problems.rules.AbstractTaintPropagationRule;
 import soot.jimple.infoflow.sourcesSinks.manager.IReversibleSourceSinkManager;
 import soot.jimple.infoflow.sourcesSinks.manager.SinkInfo;
+import soot.jimple.infoflow.util.BaseSelector;
 import soot.jimple.infoflow.util.ByReferenceBoolean;
 import soot.jimple.infoflow.util.TypeUtils;
 
@@ -35,15 +39,6 @@ public class BackwardsStrongUpdatePropagationRule extends AbstractTaintPropagati
 		AssignStmt assignStmt = (AssignStmt) stmt;
 		Value leftOp = assignStmt.getLeftOp();
 
-		// if leftvalue contains the tainted value -> it is overwritten - remove taint:
-		// but not for arrayRefs:
-		// x[i] = y --> taint is preserved since we do not distinguish between elements
-		// of collections
-		// because we do not use a MUST-Alias analysis, we cannot delete aliases of
-		// taints
-		if (leftOp instanceof ArrayRef)
-			return null;
-
 		// If this is a newly created alias at this statement, we don't kill it right
 		// away
 		if (source.getCurrentStmt() == stmt)
@@ -54,9 +49,16 @@ public class BackwardsStrongUpdatePropagationRule extends AbstractTaintPropagati
 				&& source.isAbstractionActive() && source.getAccessPath().equals(source.getPredecessor().getAccessPath()))
 			return null;
 
-		if (!(assignStmt.getRightOp() instanceof Constant))
+		// We already handle the cases where it may aliases, so no need to go further
+		if (getAliasing().mayAlias(leftOp, source.getAccessPath().getPlainValue()))
 			return null;
 
+//		if (!(assignStmt.getRightOp() instanceof Constant))
+//			return null;
+
+		boolean addRightValue = false;
+		boolean cutFirstField = false;
+		Type type = null;
 		if (source.getAccessPath().isInstanceFieldRef()) {
 			// Data Propagation: x.f = y && x.f tainted --> no taint propagated
 			// Alias Propagation: Only kill the alias if we directly overwrite it,
@@ -66,38 +68,71 @@ public class BackwardsStrongUpdatePropagationRule extends AbstractTaintPropagati
 				if (getAliasing().mustAlias((Local) leftRef.getBase(),
 						source.getAccessPath().getPlainValue(), assignStmt)) {
 					if (getAliasing().mustAlias(leftRef.getField(), source.getAccessPath().getFirstField())) {
-						killSource.value = true;
-						return null;
+						addRightValue = true;
+						cutFirstField = true;
+						type = leftRef.getField().getType();
 					}
 				}
 			}
 			// x = y && x.f tainted -> no taint propagated. This must only check the precise
 			// variable which gets replaced, but not any potential strong aliases
 			else if (leftOp instanceof Local) {
-				if (getAliasing().mustAlias((Local) leftOp, source.getAccessPath().getPlainValue(),
-						stmt)) {
-					killSource.value = true;
-					return null;
+				if (getAliasing().mustAlias((Local) leftOp, source.getAccessPath().getPlainValue(), stmt)) {
+					addRightValue = true;
+//					type = leftOp.getType();
 				}
 			}
 		}
 //		 X.f = y && X.f tainted -> no taint propagated. Kills are allowed even if
 //		 static field tracking is disabled
-		else if (source.getAccessPath().isStaticFieldRef()) {
-			if (leftOp instanceof StaticFieldRef && getAliasing().mustAlias(
-					((StaticFieldRef) leftOp).getField(), source.getAccessPath().getFirstField())) {
-				killSource.value = true;
-				return null;
+		else if (source.getAccessPath().isStaticFieldRef() && leftOp instanceof StaticFieldRef) {
+			StaticFieldRef leftRef = (StaticFieldRef) leftOp;
+			if (getAliasing().mustAlias(leftRef.getField(), source.getAccessPath().getFirstField())) {
+				addRightValue = true;
+				cutFirstField = true;
+				type = leftRef.getField().getType();
 			}
-
 		}
 		// when the fields of an object are tainted, but the base object is overwritten
 		// then the fields should not be tainted any more
 		// x = y && x.f tainted -> no taint propagated
 		else if (source.getAccessPath().isLocal() && leftOp instanceof Local
-				&& leftOp == source.getAccessPath().getPlainValue()) {
-			killSource.value = true;
-			return null;
+				&& getAliasing().mustAlias((Local) leftOp, source.getAccessPath().getPlainValue(), stmt)) {
+			addRightValue = true;
+//			type = assignStmt.getRightOp().getType();
+		}
+
+		// if leftvalue contains the tainted value -> it is overwritten - remove taint:
+		// but not for arrayRefs:
+		// x[i] = y --> taint is preserved since we do not distinguish between elements
+		// of collections
+		// because we do not use a MUST-Alias analysis, we cannot delete aliases of
+		// taints
+		if (addRightValue) {
+			killSource.value = !(leftOp instanceof ArrayRef);
+
+			Value rightOp = assignStmt.getRightOp();
+			Value rightVal = BaseSelector.selectBase(assignStmt.getRightOp(), true);
+			if (rightVal instanceof Constant || rightOp instanceof AnyNewExpr)
+				return null;
+
+			AccessPath newAp = manager.getAccessPathFactory().copyWithNewValue(source.getAccessPath(), rightVal,
+					type, cutFirstField);
+			Abstraction newAbs = source.deriveNewAbstraction(newAp, assignStmt);
+
+			if (newAbs != null) {
+				if (type instanceof PrimType || TypeUtils.isStringType(type))
+					newAbs.setTurnUnit(assignStmt);
+				else {
+					if (getAliasing().canHaveAliasesRightSide(assignStmt, rightVal, newAbs)) {
+						for (Unit pred : manager.getICFG().getPredsOf(assignStmt))
+							getAliasing().computeAliases(d1, (Stmt) pred, rightVal, Collections.singleton(newAbs),
+									manager.getICFG().getMethodOf(pred), newAbs);
+					}
+				}
+
+				return Collections.singleton(newAbs);
+			}
 		}
 
 		return null;
@@ -106,6 +141,53 @@ public class BackwardsStrongUpdatePropagationRule extends AbstractTaintPropagati
 	@Override
 	public Collection<Abstraction> propagateCallFlow(Abstraction d1, Abstraction source, Stmt stmt, SootMethod dest,
 													 ByReferenceBoolean killAll) {
+		if (!(stmt instanceof AssignStmt))
+			return null;
+
+		AssignStmt assignStmt = (AssignStmt) stmt;
+		Value left = assignStmt.getLeftOp();
+
+		Aliasing aliasing = getAliasing();
+		if (aliasing == null)
+			return null;
+
+		// We already handle the cases where it may aliases, so no need to go further
+		if (aliasing.mayAlias(left, source.getAccessPath().getPlainValue()))
+			return null;
+
+		if (source.getAccessPath().isStaticFieldRef())
+			return null;
+
+		// we only taint the return statement(s) if left is tainted
+		if (aliasing.mustAlias((Local) left, source.getAccessPath().getPlainValue(), stmt)) {
+			HashSet<Abstraction> res = new HashSet<>();
+			for (Unit unit : dest.getActiveBody().getUnits()) {
+				if (unit instanceof ReturnStmt) {
+					ReturnStmt returnStmt = (ReturnStmt) unit;
+					Value retVal = returnStmt.getOp();
+					// taint only if local variable or reference
+					if (retVal instanceof Local || retVal instanceof FieldRef) {
+						// if types are incompatible, stop here
+						if (!manager.getTypeUtils().checkCast(source.getAccessPath().getBaseType(), retVal.getType()))
+							continue;
+
+						Type type = returnStmt.getOp().getType();
+						AccessPath ap = manager.getAccessPathFactory().copyWithNewValue(
+								source.getAccessPath(), retVal, type, false);
+						Abstraction abs = source.deriveNewAbstraction(ap, stmt);
+						if (abs != null) {
+							if (type instanceof PrimType || TypeUtils.isStringType(type))
+								abs.setTurnUnit(stmt);
+							else
+								abs.setAliasingFlag((Stmt) assignStmt);
+
+							res.add(abs);
+						}
+					}
+				}
+			}
+			return res;
+		}
 		return null;
 	}
 
