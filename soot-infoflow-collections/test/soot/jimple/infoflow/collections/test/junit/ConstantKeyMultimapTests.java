6111d98d388616c25c8a702704927ed51bc7b3e5
Tim Lange <mail@tim-lange.me>
2023-05-24 16:48:07 +0200

    progress

---
+++ soot-infoflow-collections/test/soot/jimple/infoflow/collections/test/junit/ConstantKeyMultimapTests.java
@@ -0,0 +1,45 @@
+package soot.jimple.infoflow.collections.test.junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import soot.jimple.infoflow.IInfoflow;
+import soot.jimple.infoflow.InfoflowConfiguration;
+import soot.jimple.infoflow.util.DebugFlowFunctionTaintPropagationHandler;
+
+import java.util.Collections;
+
+public class ConstantKeyMultimapTests extends FlowDroidTests {
+    @Override
+    protected void setConfiguration(InfoflowConfiguration config) {
+
+    }
+
+    private static final String testCodeClass = "soot.jimple.infoflow.collections.test.ConstantKeyMultimapTestCode";
+
+    @Test(timeout = 30000)
+    public void testMultimapPutGet1() {
+        IInfoflow infoflow = initInfoflow();
+//        infoflow.getConfig().setWriteOutputFiles(true);
+        infoflow.setTaintPropagationHandler(new DebugFlowFunctionTaintPropagationHandler());
+        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+        infoflow.getConfig().getPathConfiguration().setPathReconstructionMode(InfoflowConfiguration.PathReconstructionMode.Fast);
+        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+        Assert.assertEquals(1, infoflow.getResults().size());
+    }
+
+    @Test(timeout = 30000)
+    public void testMultimapPutGet2() {
+        IInfoflow infoflow = initInfoflow();
+        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+        Assert.assertEquals(0, infoflow.getResults().size());
+    }
+
+    @Test(timeout = 30000)
+    public void testMultimapPutGet3() {
+        IInfoflow infoflow = initInfoflow();
+        String epoint = "<" + testCodeClass + ": void " + getCurrentMethod() + "()>";
+        infoflow.computeInfoflow(appPath, libPath, Collections.singleton(epoint), sources, sinks);
+        Assert.assertEquals(0, infoflow.getResults().size());
+    }
+}
