89b78d9c348abf3cb8131722040da24d48e93884
Tim Lange <mail@tim-lange.me>
2023-08-18 15:04:44 +0200

    Fix

--- soot-infoflow-summaries/src/soot/jimple/infoflow/methodSummary/data/provider/XMLSummaryProvider.java
+++ soot-infoflow-summaries/src/soot/jimple/infoflow/methodSummary/data/provider/XMLSummaryProvider.java
@@ -49,7 +49,7 @@ public abstract class XMLSummaryProvider extends AbstractMethodSummaryProvider {
 
 	protected final static String FILE_META_DATA = "SummaryMetaData.xml";
 
-	protected SummaryReader summaryReader;
+	protected SummaryReader summaryReader = new SummaryReader();
 	protected MetaDataReader metaDataReader = new MetaDataReader();
 
 	protected ClassSummaries summaries = createClassSummaries();
