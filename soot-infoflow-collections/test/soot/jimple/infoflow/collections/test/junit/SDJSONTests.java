aacf82f5fb789682608eca2571033d73a9f51470
Tim Lange <mail@tim-lange.me>
2023-09-26 17:44:15 +0200

    morgen bundle, dann eval

---
+++ soot-infoflow-collections/test/soot/jimple/infoflow/collections/test/junit/SDJSONTests.java
@@ -0,0 +1,28 @@
+package soot.jimple.infoflow.collections.test.junit;
+
+import java.io.File;
+import java.util.Collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import soot.jimple.infoflow.IInfoflow;
+import soot.jimple.infoflow.InfoflowConfiguration;
+import soot.jimple.infoflow.collections.StubDroidBasedTaintWrapper;
+import soot.jimple.infoflow.collections.parser.StubDroidSummaryProvider;
+import soot.jimple.infoflow.collections.test.JSONTestCode;
+import soot.jimple.infoflow.taintWrappers.ITaintPropagationWrapper;
+
+public class SDJSONTests extends JSONTests {
+
+    @Override
+    protected ITaintPropagationWrapper getTaintWrapper() {
+        try {
+            StubDroidSummaryProvider sp = new StubDroidSummaryProvider(new File("stubdroidBased"));
+            sp.loadAdditionalSummaries("summariesManual");
+            return new StubDroidBasedTaintWrapper(sp);
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+}
