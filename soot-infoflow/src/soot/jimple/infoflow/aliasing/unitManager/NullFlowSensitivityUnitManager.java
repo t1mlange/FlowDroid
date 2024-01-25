0b0ee2cdd6ce9004ad463284a8aa95fadc1d166f
Tim Lange <mail@tim-lange.me>
2024-01-25 10:43:58 +0100

    wip

--- soot-infoflow/src/soot/jimple/infoflow/aliasing/unitManager/NullFlowSensitivityUnitManager.java
+++ soot-infoflow/src/soot/jimple/infoflow/aliasing/unitManager/NullFlowSensitivityUnitManager.java
@@ -1,7 +1,8 @@
-package soot.jimple.infoflow.aliasing;
+package soot.jimple.infoflow.aliasing.unitManager;
 
 import soot.SootMethod;
 import soot.Unit;
+import soot.jimple.infoflow.aliasing.IFlowSensitivityUnitManager;
 import soot.jimple.infoflow.data.Abstraction;
 
 public class NullFlowSensitivityUnitManager implements IFlowSensitivityUnitManager {
